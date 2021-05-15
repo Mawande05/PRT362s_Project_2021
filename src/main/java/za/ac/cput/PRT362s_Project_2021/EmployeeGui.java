e359a780cd1d9a0a3c4e099d5f8cc61922341734
Asive madladla <asivemadladla@gmail.com>
2021-05-15 18:01:32 +0200

    Made changes in my Gui

--- src/main/java/za/ac/cput/PRT362s_Project_2021/EmployeeGui.java
+++ src/main/java/za/ac/cput/PRT362s_Project_2021/EmployeeGui.java
@@ -1,180 +1,157 @@
 package za.ac.cput.PRT362s_Project_2021;
 import za.ac.cput.PRT362s_Project_2021.Employee.Employee;
 
+import java.io.*;
 import java.awt.*;
-import java.awt.event.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import javax.swing.*;
+import java.awt.event.*;
+import java.sql.*;
+import java.util.*;
 
-public class EmployeeGui implements ActionListener {
-    private Frame mainframe;
-    private JPanel panelNorth, panelWest, panelEast, panelSouth;
-    private JLabel lblLogo;
-    private JLabel lblHeading;
-    private JLabel lblId, lblName, lblDepartment, lblSalary;
-    private JTextField txtId, txtName, txtDepartment, txtSalary;
-    private JButton btnAdd, btnDelete, btnUpdate;
-    private Font ft1, ft2;
+class EmployeeInformation {
+    JFrame mainframe;
+    JPanel panel1,panel2,panel3;
+    JTabbedPane tp;
+    JLabel lblLogo,lblId, lblName, lblAddress, lblSalary,lbl5,lbl6,lbl7,lbl8,lbl9,lbl10;
+    JTextField tf1,tf2,tf3,tf4,tf5,tf6,tf7,tf8,tf9,tf10;
+    JButton btnSave,btnReset,btnEdit1,btnEdit2,btnDelete ;
 
-    public EmployeeGui() {
-        mainframe = new Frame("Employee Application");
-        panelNorth = new JPanel();
-        panelWest = new JPanel();
-        panelEast = new JPanel();
-        panelSouth = new JPanel();
+    EmployeeInformation() {
+        mainframe = new JFrame("Employee Application");
+        panel1 = new JPanel(new GridLayout(5, 2));
+        panel2 = new JPanel(new GridLayout(5, 2));
+        panel3 = new JPanel(new GridLayout(2, 2));
+        tp = new JTabbedPane();
+        lblId = new JLabel("Employee ID:");
+        lblName = new JLabel("Employee Name:");
+        lblAddress = new JLabel("Employee Address:");
+        lblSalary = new JLabel("Salary:");
+        lbl5 = new JLabel("Enter Employee ID to delete record:");
 
-        lblHeading = new JLabel("Add Employee Details");
-
-        lblId = new JLabel("ID: ");
-        lblName = new JLabel("Name: ");
-        lblDepartment = new JLabel("Department: ");
-        lblSalary = new JLabel("Salary: ");
-        txtId = new JTextField();
-        txtName = new JTextField();
-        txtDepartment = new JTextField();
-        txtSalary = new JTextField();
-        btnAdd = new JButton("Add");
+        lbl7 = new JLabel("Employee ID:");
+        lbl8 = new JLabel("Employee Name:");
+        lbl9 = new JLabel("Employee Address:");
+        lbl10 = new JLabel("Salary:");
+        tf1 = new JTextField(12);
+        tf2 = new JTextField(12);
+        tf3 = new JTextField(12);
+        tf4 = new JTextField(12);
+        tf5 = new JTextField(12);
+        tf6 = new JTextField(12);
+        tf7 = new JTextField(12);
+        tf8 = new JTextField(12);
+        tf9 = new JTextField(12);
+        tf10 = new JTextField(12);
+        btnSave = new JButton(" Add ");
+        btnReset = new JButton(" Reset");
+        btnEdit1 = new JButton(" Edit ");
+        btnEdit2 = new JButton(" Save");
         btnDelete = new JButton("Delete");
-        btnUpdate = new JButton("Update");
 
-        ft1 = new Font("Arial", Font.BOLD, 32);
-        ft2 = new Font("Arial", Font.PLAIN, 22);
+        panel1.setBackground(new Color(88, 220, 204));
 
-    }
+        panel1.add(lblId);
+        panel1.add(tf1);
+        panel1.add(lblName);
+        panel1.add(tf2);
+        panel1.add(lblAddress);
+        panel1.add(tf3);
+        panel1.add(lblSalary);
+        panel1.add(tf4);
+        panel1.add(btnSave);
+        panel1.add(btnReset);
 
-    public void setGUI() {
-        panelNorth.setLayout(new FlowLayout());
-        panelEast.setPreferredSize(new Dimension(700, 130));
-        panelWest.setLayout(new GridLayout(4, 1));
-        panelEast.setLayout(new GridLayout(4, 1));
-        panelSouth.setLayout(new GridLayout(1, 3));
+        panel2.add(lbl7);
+        panel2.add(tf7);
+        panel2.add(lbl8);
+        panel2.add(tf8);
+        panel2.add(lbl9);
+        panel2.add(tf9);
+        panel2.add(lbl10);
+        panel2.add(tf10);
+        panel2.add(btnEdit1);
+        panel2.add(btnEdit2);
 
-
-        panelNorth.add(lblHeading);
-        lblHeading.setFont(ft1);
-        lblHeading.setForeground(Color.yellow);
-        panelNorth.setBackground(Color.LIGHT_GRAY);
-
-        lblId.setFont(ft2);
-        lblName.setFont(ft2);
-        lblDepartment.setFont(ft2);
-        lblSalary.setFont(ft2);
-        txtId.setFont(ft2);
-        txtName.setFont(ft2);
-        txtDepartment.setFont(ft2);
-        txtSalary.setFont(ft2);
-        btnAdd.setFont(ft2);
-        btnDelete.setFont(ft2);
-        btnUpdate.setFont(ft2);
-
-        panelWest.add(lblId);
-        panelWest.add(lblName);
-        panelWest.add(lblDepartment);
-        panelWest.add(lblSalary);
-        panelEast.add(txtId);
-        panelEast.add(txtName);
-        panelEast.add(txtDepartment);
-        panelEast.add(txtSalary);
-        panelSouth.add(btnAdd);
-        panelSouth.add(btnDelete);
-        panelSouth.add(btnUpdate);
-
-        mainframe.add(panelNorth, BorderLayout.NORTH);
-        mainframe.add(panelWest, BorderLayout.WEST);
-        mainframe.add(panelEast, BorderLayout.EAST);
-        mainframe.add(panelSouth, BorderLayout.SOUTH);
-        mainframe.addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
+        panel3.add(lbl5);
+        panel3.add(tf5);
+        panel3.add(btnDelete);
+        btnReset.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                tf1.setText("");
+                tf2.setText("");
+                tf3.setText("");
+                tf4.setText("");
             }
         });
-        btnAdd.addActionListener(this);
-        btnDelete.addActionListener(this);
-        btnUpdate.addActionListener(this);
-        mainframe.pack();
-        mainframe.setVisible(true);
-        txtId.requestFocus();
-    }
-    public void clear() {
-        txtId.setText(" ");
-        txtName.setText(" ");
-        txtDepartment.setText(" ");
-        txtSalary.setText(" ");
-        txtId.requestFocus();
-    }
-
-
-
-    public boolean isValid() {
-        boolean valid = true;
-
-        if (txtSalary.getText().equals("")) {
-            txtSalary.setText("Invalid Salary");
-            txtSalary.requestFocus(true);
-            valid = false;
-        }
-
-        if (txtName.getText().equals("")) {
-            txtName.setText("Invalid Name");
-            txtName.requestFocus(true);
-            valid = false;
-        }
-
-        if (txtDepartment.getText().equals("")) {
-            txtDepartment.setText("Invalid Department");
-            txtDepartment.requestFocus(true);
-            valid = false;
-        }
-
-        if (txtId.getText().equals("")) {
-            txtId.setText("Invalid Id");
-            txtId.requestFocus(true);
-            valid = false;
-        }
-
-        return valid;
-    }
-
-    public void actionPerformed(ActionEvent e) {
-        btnAdd.addActionListener(new ActionListener() {
+        btnSave.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
-                String value1 = txtId.getText();
-                String value2 = txtName.getText();
-                String value3 = txtDepartment.getText();
-                String value4 = txtSalary.getText();
+                String value1 = tf1.getText();
+                String value2 = tf2.getText();
+                String value3 = tf3.getText();
+                String value4 = tf4.getText();
                 Connection con = null;
                 String url = "jdbc:mysql://localhost:3306/";
                 String db = "test";
                 String driver = "com.mysql.jdbc.Driver";
-                String user = "root";
-                String pass = "root";
+                String username = "root";
+                String password = " ";
                 System.out.println(value1 + value2 + value3 + value4);
                 try {
                     Class.forName(driver);
-                    con = DriverManager.getConnection(url + db, user, pass);
-                    PreparedStatement st = con.prepareStatement("insert into employee(emp_id,emp_name,emp_department,salary) values(?,?,?,?)");
+                    con = DriverManager.getConnection(url + db, username, password);
+                    PreparedStatement st = con.prepareStatement("insert into employee(emp_id,emp_name,emp_address,salary) values(?,?,?,?)");
                     st.setString(1, value1);
                     st.setString(2, value2);
                     st.setString(3, value3);
                     st.setString(4, value4);
                     st.executeUpdate();
-                    JOptionPane.showMessageDialog(mainframe, "Data is successfully inserted into database.");
+                    JOptionPane.showMessageDialog(panel1, "Data is successfully inserted into database.");
                     con.close();
                 } catch (Exception e) {
-                    JOptionPane.showMessageDialog(mainframe, "Error in submitting data!");
+                    JOptionPane.showMessageDialog(panel1, "Error in submitting data!");
+                }
+            }
+        });
+
+        btnDelete.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+
+                String value1 = tf5.getText();
+                Connection con = null;
+                String url = "jdbc:mysql://localhost:3306/";
+                String db = "test";
+                String driver = "com.mysql.jdbc.Driver";
+                String username = "root";
+                String password = " ";
+                try {
+                    Class.forName(driver);
+                    con = DriverManager.getConnection(url + db, username, password);
+                    PreparedStatement st = con.prepareStatement("DELETE FROM employee WHERE emp_id = ?");
+                    st.setString(1, value1);
+                    st.executeUpdate();
+                    JOptionPane.showMessageDialog(panel3, "Record is deleted successfully.");
+                    con.close();
+                } catch (Exception exp3) {
+                    JOptionPane.showMessageDialog(panel3, "Error in deleting record.");
                 }
             }
         });
 
     }
-    public static void main(String[] args) {
-        new EmployeeGui().setGUI();
+    void dis()
+    {
+        mainframe.getContentPane().add(tp);
+        tp.addTab("Add Record",panel1);
+        tp.addTab("Edit Record",panel2);
+        tp.addTab("Delete Record",panel3);
+
+        mainframe.setSize(450,280);
+        mainframe.setVisible(true);
+        mainframe.setResizable(true);
     }
-
+    public static void main(String z[]){
+        EmployeeInformation pro=new EmployeeInformation();
+        pro.dis();
+    }
 }
-
-
-
 
